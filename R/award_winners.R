78a7fe156a446d3b794059f5721e43a00ff28a82
jhavstad8 <jhavstad@calpoly.edu>
2022-06-07 23:34:05 -0700

    updates

--- R/award_winners.R
+++ R/award_winners.R
@@ -8,11 +8,12 @@
 #' @import ggplot2
 #' @import gganimate
 #' @import rlang
+#' @return An animated plot
 #' @export
 
 
 award_winners <- function(award,stat,start = NULL, end = NULL){
-  # specify which dataset to work with
+  # specify which data set to work with
 if(award == "MVP"){
   dat <- read_mvps(mvp)
 }
@@ -57,3 +58,37 @@ else if(award == "MIP"){
 
 
 
+
+#' Compares multiple awards at a time
+#'
+#' @param awards A vector of awards you wish to compare
+#' @param stat The stat you want to focus on
+#' @import ggplot2
+#' @import gganimate
+#' @import reshape2
+#' @import tidyverse
+#' @import rvest
+#'
+#' @return an animated plot
+#'
+#' @export
+
+
+comp_awards <- function(awards, stat){
+  # specify which data set to work with
+  if(award == "MVP"){
+    dat <- read_mvps(mvp)
+  }
+  else if(award == "DPOY"){
+    dat <- read_dpoy(dpoy)
+  }
+  else if(award == "SMOY"){
+    dat <- read_smoy(smoy)
+  }
+  else if(award == "ROY"){
+    dat <- read_roys(roy)
+  }
+  else if(award == "MIP"){
+    dat <- read_mip(mip)
+  }
+}
