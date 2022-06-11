d992b1a476da48d1410b52407159ebfe2db42b44
jhavstad8 <jhavstad@calpoly.edu>
2022-06-10 17:35:46 -0700

    updates

--- R/Untitled.R
+++
@@ -1,9 +0,0 @@
-#' Combine stats and compare players
-#'
-#' @param award The award to compare players for (auoted)
-#' @param stats Vector of stats to combine (quoted)
-#' @param start Season to start at
-#' @param end Season to end at
-#' @import tidyverse
-#' @return A data frame with rankings of players
-#' @export
